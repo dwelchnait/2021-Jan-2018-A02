e0539866d58de2e4005c3bc1caf1e19504abdc34
Don Welch <dwelch@nait.ca>
2021-02-26 16:22:53 -0700

    ManagePlaylist task:Fetch by Artist

--- ChinookSolution/ChinookSystem/BLL/TrackController.cs
+++ ChinookSolution/ChinookSystem/BLL/TrackController.cs
@@ -54,7 +54,20 @@ namespace ChinookSystem.BLL
             {
                 List<TrackList> results = null;
 
-               //code to go here
+                results = (from x in context.Tracks
+                          where x.Album.Artist.Name.Contains(arg)
+                          select new TrackList
+                          {
+                              TrackID = x.TrackId,
+                              Name = x.Name,
+                              Title = x.Album.Title,
+                              ArtistName = x.Album.Artist.Name,
+                              GenreName = x.Genre.Name,
+                              Composer = x.Composer,
+                              Milliseconds = x.Milliseconds,
+                              Bytes = x.Bytes,
+                              UnitPrice = x.UnitPrice
+                          }).ToList();
 
                 return results;
             }
