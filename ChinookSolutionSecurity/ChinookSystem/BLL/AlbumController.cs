69ec84b53fd5ac9c1f44ddbb75ec4998779073ae
Don Welch <dwelch@nait.ca>
2021-03-17 13:49:22 -0600

    Copy of non Chinook Solution sample

---
+++ ChinookSolutionSecurity/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.DAL;
+using ChinookSystem.Entities;   //for Sql and are internal
+using ChinookSystem.ViewModels; //for data class to transfer data from BLL to web app
+using System.ComponentModel;    //for ODS wizard
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        #region Queries
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ArtistAlbums> Albums_GetArtistAlbums()
+        {
+            using(var context = new ChinookSystemContext())
+            {
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name
+                                                    };
+                return results.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ArtistAlbums> Albums_GetAlbumsForArtist(int artistid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    where x.ArtistId == artistid
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name,
+                                                        ArtistId = x.ArtistId
+                                                    };
+                return results.ToList();
+            }
+        }
+
+
+        //query to return all data of the Album table
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<AlbumItem> Albums_List()
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                IEnumerable<AlbumItem> results = from x in context.Albums
+                                                    select new AlbumItem
+                                                    {
+                                                        AlbumId = x.AlbumId,
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistId = x.ArtistId,
+                                                        ReleaseLabel = x.ReleaseLabel
+                                                    };
+                return results.ToList();
+            }
+        }
+
+        //query to look up an Album record by pkey
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public AlbumItem Albums_FindById(int albumid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                // (...).FirstOrDefault will return either
+                //    a) the first record matching the where condition
+                //    b) a null value
+                AlbumItem results = (from x in context.Albums
+                              where x.AlbumId == albumid
+                            select new AlbumItem
+                            {
+                                AlbumId = x.AlbumId,
+                                Title = x.Title,
+                                ReleaseYear = x.ReleaseYear,
+                                ArtistId = x.ArtistId,
+                                ReleaseLabel = x.ReleaseLabel
+                            }).FirstOrDefault();
+                return results;
+            }
+        }
+
+        #endregion
+
+        #region Add, Update and Delete CRUD
+
+        //Add
+        [DataObjectMethod(DataObjectMethodType.Insert,false)]
+        public int Album_Add(AlbumItem item)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                //due to the fact that we have separated the handling of our entities
+                //   from the data transfer between web app and class library
+                //   using the ViewModel classes, we MUST create an instance
+                //   of the entity and move the data from the ViewModel class
+                //   to the entity instance
+                Album addItem = new Album
+                {
+                    //why no pkey set?
+                    //pkey is an identity pkey, no value is needed
+                    Title = item.Title,
+                    ArtistId = item.ArtistId,
+                    ReleaseYear = item.ReleaseYear,
+                    ReleaseLabel = item.ReleaseLabel
+                };
+
+                //staging 
+                //setup in local memory
+                //at this point you will NOT have sent anything to the database
+                //      therefore, you will NOT have your new pkey as yet
+                context.Albums.Add(addItem);
+
+                //commit to database
+                //on this command you
+                //  a) execute entity validation annotation
+                //  b) send your local memory staging to the database for execution
+                //after a successful execution your entity instance will have the
+                //      new pkey (Identity) value
+                context.SaveChanges();
+
+                //at this point, your entity instance has the new pkey value
+                return addItem.AlbumId;
+            }
+        }
+        //Update
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
+        public void Album_Update(AlbumItem item)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                //due to the fact that we have separated the handling of our entities
+                //   from the data transfer between web app and class library
+                //   using the ViewModel classes, we MUST create an instance
+                //   of the entity and move the data from the ViewModel class
+                //   to the entity instance
+                Album updateItem = new Album
+                {
+                    //for an update, you need to supply your pkey value
+                    AlbumId = item.AlbumId,
+                    Title = item.Title,
+                    ArtistId = item.ArtistId,
+                    ReleaseYear = item.ReleaseYear,
+                    ReleaseLabel = item.ReleaseLabel
+                };
+
+                //staging 
+                //setup in local memory
+
+                context.Entry(updateItem).State = System.Data.Entity.EntityState.Modified;
+
+                //commit to database
+                //on this command you
+                //  a) execute entity validation annotation
+                //  b) send your local memory staging to the database for execution
+                
+                context.SaveChanges();
+
+            }
+        }
+        //Delete
+
+        //When we do an ODS CRUD on the delete, the ODS sends in the entire
+        //    instance record, not just the pkey value
+
+        //overload the Album_Delete method so it receive a whole instance
+        //    then call the actual delete method passing just the
+        //    pkey value to the actual delete method
+        [DataObjectMethod(DataObjectMethodType.Delete, false)]
+        public void Album_Delete(AlbumItem item)
+        {
+            Album_Delete(item.AlbumId);
+        }
+
+        public void Album_Delete(int albumid)
+        {
+            using(var context = new ChinookSystemContext())
+            {
+                //example of a phyiscal delete
+                //retreive the current entity instance based on the incoming parameter
+                var exists = context.Albums.Find(albumid);
+                //staged the remove
+                context.Albums.Remove(exists);
+                //commit the remove
+                context.SaveChanges();
+
+                //a logical delete is actually an update of the instance
+            }
+        }
+        #endregion
+    }
+}
---
+++ ChinookSolutionSecurity/WebApp/SamplePages/ManagePlaylist.aspx.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additonal Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.ViewModels;
+
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class ManagePlaylist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TracksSelectionList.DataSource = null;
+        }
+
+        #region MessageUserControl Error Handling for ODS
+        protected void SelectCheckForException(object sender,
+             ObjectDataSourceStatusEventArgs e)
+        {
+            MessageUserControl.HandleDataBoundException(e);
+        }
+        protected void InsertCheckForException(object sender,
+             ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process success", "Album has been added");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+
+        }
+        protected void UpdateCheckForException(object sender,
+             ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process success", "Album has been updated");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+
+        }
+        protected void DeleteCheckForException(object sender,
+             ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process success", "Album has been removed");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+
+        }
+        #endregion
+
+
+        protected void ArtistFetch_Click(object sender, EventArgs e)
+        {
+            TracksBy.Text = "Artist";
+            //the HiddenField content access is .Value NOT .Text
+            if (string.IsNullOrEmpty(ArtistName.Text))
+            {
+                MessageUserControl.ShowInfo("You did not supply an artist name");
+                //the HiddenField content access is .Value NOT .Text
+                SearchArg.Value = "zxcvg";
+            }
+            else
+            {
+                //the HiddenField content access is .Value NOT .Text
+                SearchArg.Value = ArtistName.Text;
+            }
+            //to force the re-execution of an ODS attached to a display control
+            //    rebind the display control
+            TracksSelectionList.DataBind();
+        }
+
+
+        protected void GenreFetch_Click(object sender, EventArgs e)
+        {
+            TracksBy.Text = "Genre";
+
+            //if you had a prompt on your dropdownlist, you would verify that a 
+            //   selection was made
+
+            //you could use the value field of the dropdownlist
+            //SearchArg.Value = GenreDDL.SelectedValue;
+
+            //Can I use something else from the dropdownlist instead of the value field???
+            //there is the display field
+            //WARNING using the display field for the local, in this example, is possible 
+            //   because EACH description is unique!!!!!
+            SearchArg.Value = GenreDDL.SelectedItem.Text;
+
+            //to force the re-execution of an ODS attached to a display control
+            //    rebind the display control
+            TracksSelectionList.DataBind();
+        }
+
+        protected void AlbumFetch_Click(object sender, EventArgs e)
+        {
+            TracksBy.Text = "Album";
+            //the HiddenField content access is .Value NOT .Text
+            if (string.IsNullOrEmpty(AlbumTitle.Text))
+            {
+                MessageUserControl.ShowInfo("You did not supply an album title");
+                //the HiddenField content access is .Value NOT .Text
+                SearchArg.Value = "zxcvg";
+            }
+            else
+            {
+                //the HiddenField content access is .Value NOT .Text
+                SearchArg.Value = AlbumTitle.Text;
+            }
+            //to force the re-execution of an ODS attached to a display control
+            //    rebind the display control
+            TracksSelectionList.DataBind();
+        }
+
+        protected void PlayListFetch_Click(object sender, EventArgs e)
+        {
+            //username is coming from the system via security
+            //since security has yet to be installed, a defualt will be setup for the
+            //   username value
+            string username = "HansenB";
+            if (string.IsNullOrEmpty(PlaylistName.Text))
+            {
+                MessageUserControl.ShowInfo("Playlist Search", "No playlist name was supplied.");
+            }
+            else
+            {
+                //use some user friendly error handling
+                //the way we are doing the error handling is using MessageUserControl instead
+                //  of try/catch
+                //MessageUserControl has the try/catch embedded within the control logic
+                //within the MessageUserControl there exists a method called .TryRun()
+                //syntax
+                //   MessageUserControl.TryRun( () => {
+                //
+                //     your coding logic
+                //
+                //  }[,"message title","success message"]);
+                //
+                MessageUserControl.TryRun(() => { 
+                    PlaylistTracksController sysmgr = new PlaylistTracksController();
+                RefreshPlayList(sysmgr, username);
+
+                },"Playlist Search","View the requested playlist below");
+               
+            }
+ 
+        }
+
+        protected void RefreshPlayList(PlaylistTracksController sysmgr, string username)
+        {
+            List<UserPlaylistTrack> info = sysmgr.List_TracksForPlaylist(
+                       PlaylistName.Text, username);
+            PlayList.DataSource = info;
+            PlayList.DataBind();
+        }
+
+        protected void MoveDown_Click(object sender, EventArgs e)
+        {
+            //form event validation: presence
+            if (string.IsNullOrEmpty(PlaylistName.Text))
+            {
+                MessageUserControl.ShowInfo("Missing Data", "Enter a playlist name");
+            }
+            else
+            {
+                if (PlayList.Rows.Count == 0)
+                {
+                    MessageUserControl.ShowInfo("Track Movement", "You must have a play list visible to choose tracks for movement. Select from the displayed playlist.");
+                }
+                else
+                {
+                    MoveTrackItem moveTrack = new MoveTrackItem();
+                    int rowsSelected = 0;
+                    CheckBox trackSelection = null;
+                    //traverse the gridview control PlayList
+                    //you could do this same code using a foreah()
+                    for (int i = 0; i < PlayList.Rows.Count; i++)
+                    {
+                        //point to the checkbox control on the gridview row
+                        trackSelection = PlayList.Rows[i].FindControl("Selected") as CheckBox;
+                        //test the setting of the checkbox
+                        if (trackSelection.Checked)
+                        {
+                            rowsSelected++;
+                            moveTrack.TrackID = int.Parse((trackSelection.FindControl("TrackId") as Label).Text);
+                            moveTrack.TrackNumber = int.Parse((trackSelection.FindControl("TrackNumber") as Label).Text);
+                        }
+                    }
+
+                    //was a single song selected
+                    switch (rowsSelected)
+                    {
+                        case 0:
+                            {
+                                MessageUserControl.ShowInfo("Track Movement", "You must select   one song to move.");
+                                break;
+                            }
+                        case 1:
+                            {
+                                //rule: do not move if last song
+                                if (moveTrack.TrackNumber == PlayList.Rows.Count)
+                                {
+                                    MessageUserControl.ShowInfo("Track Movement", "Song select is already the last song. Moving down not necessary.");
+                                }
+                                else
+                                {
+                                    moveTrack.Direction = "down";
+                                    MoveTrack(moveTrack);
+                                }
+                                
+                                break;
+                            }
+                        default:
+                            {
+                                //more than 1
+                                MessageUserControl.ShowInfo("Track Movement", "You must select only one song to move.");
+                                break;
+                            }
+                    }
+                }
+            }
+        }
+
+        protected void MoveUp_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(PlaylistName.Text))
+            {
+                MessageUserControl.ShowInfo("Missing Data", "Enter a playlist name");
+            }
+            else
+            {
+                if (PlayList.Rows.Count == 0)
+                {
+                    MessageUserControl.ShowInfo("Track Movement", "You must have a play list visible to choose tracks for movement. Select from the displayed playlist.");
+                }
+                else
+                {
+                    MoveTrackItem moveTrack = new MoveTrackItem();
+                    int rowsSelected = 0;
+                    CheckBox trackSelection = null;
+                    //traverse the gridview control PlayList
+                    //you could do this same code using a foreah()
+                    for (int i = 0; i < PlayList.Rows.Count; i++)
+                    {
+                        //point to the checkbox control on the gridview row
+                        trackSelection = PlayList.Rows[i].FindControl("Selected") as CheckBox;
+                        //test the setting of the checkbox
+                        if (trackSelection.Checked)
+                        {
+                            rowsSelected++;
+                            moveTrack.TrackID = int.Parse((trackSelection.FindControl("TrackId") as Label).Text);
+                            moveTrack.TrackNumber = int.Parse((trackSelection.FindControl("TrackNumber") as Label).Text);
+
+
+                        }
+                    }
+
+                    //was a single song selected
+                    switch (rowsSelected)
+                    {
+                        case 0:
+                            {
+                                MessageUserControl.ShowInfo("Track Movement", "You must select   one song to move.");
+                                break;
+                            }
+                        case 1:
+                            {
+                                //rule: do not move if last song
+                                if (moveTrack.TrackNumber == 1)
+                                {
+                                    MessageUserControl.ShowInfo("Track Movement", "Song select is already the first song. Moving up not necessary.");
+                                }
+                                else
+                                {
+                                    moveTrack.Direction = "up";
+                                    MoveTrack(moveTrack);
+                                }
+
+                                break;
+                            }
+                        default:
+                            {
+                                //more than 1
+                                MessageUserControl.ShowInfo("Track Movement", "You must select only one song to move.");
+                                break;
+                            }
+                    }
+                }
+            }
+
+        }
+
+        protected void MoveTrack(MoveTrackItem movetrack)
+        {
+            //call BLL to move track
+            string username = "HansenB"; //until security is implemented
+            movetrack.UserName = username;
+            movetrack.PlaylistName = PlaylistName.Text;
+
+            MessageUserControl.TryRun(() =>
+            {
+                PlaylistTracksController sysmgr = new PlaylistTracksController();
+                sysmgr.MoveTrack(movetrack);
+                RefreshPlayList(sysmgr, username);
+            },"Track Movement","Track has been moved.");
+        }
+
+
+        protected void DeleteTrack_Click(object sender, EventArgs e)
+        {
+            string username = "HansenB"; //until security is implemented
+
+            //form event validation: presence
+            if (string.IsNullOrEmpty(PlaylistName.Text))
+            {
+                MessageUserControl.ShowInfo("Missing Data", "Enter a playlist name");
+            }
+            else
+            {
+                if (PlayList.Rows.Count == 0)
+                {
+                    MessageUserControl.ShowInfo("Track Removal", "You must have a play list visible to choose removals. Select from the displayed playlist.");
+                }
+                else
+                {
+                    //collect the tracks indicated on the playlist for removal
+                    List<int> trackids = new List<int>();
+                    int rowsSelected = 0;
+                    CheckBox trackSelection = null;
+                    //traverse the gridview control PlayList
+                    //you could do this same code using a foreah()
+                    for(int i = 0; i <PlayList.Rows.Count; i++)
+                    {
+                        //point to the checkbox control on the gridview row
+                        trackSelection = PlayList.Rows[i].FindControl("Selected") as CheckBox;
+                        //test the setting of the checkbox
+                        if (trackSelection.Checked)
+                        {
+                            rowsSelected++;
+                            trackids.Add(int.Parse((PlayList.Rows[i].FindControl("TrackId") as Label).Text));
+                        }
+                    }
+
+                    //was a song selected
+                    if (rowsSelected == 0)
+                    {
+                        MessageUserControl.ShowInfo("Missing Data", "You must select at least one song to remove.");
+                    }
+                    else
+                    {
+                        //data collected, send for processing
+                        MessageUserControl.TryRun(() =>
+                        {
+                            PlaylistTracksController sysmgr = new PlaylistTracksController();
+                            sysmgr.DeleteTracks(username, PlaylistName.Text, trackids);
+                            RefreshPlayList(sysmgr,username);
+                        },"Track removal", "Selected track(s) have been removed from the playlist.");
+                    }
+                }
+            }
+        }
+
+        protected void TracksSelectionList_ItemCommand(object sender, 
+            ListViewCommandEventArgs e)
+        {
+            string username = "HansenB"; //until security is implemented
+
+            //form event validation: presence
+            if(string.IsNullOrEmpty(PlaylistName.Text))
+            {
+                MessageUserControl.ShowInfo("Missing Data", "Enter a playlist name");
+            }
+            else
+            {
+                //Reminder: MessageUserControl will do the error handling
+                MessageUserControl.TryRun(() => {
+                    //logic for your coding block
+                    PlaylistTracksController sysmgr = new PlaylistTracksController();
+                    //access a specific field on the selected ListView row
+                    string song = (e.Item.FindControl("NameLabel") as Label).Text;
+                    sysmgr.Add_TrackToPLaylist(PlaylistName.Text,
+                                                username,
+                                                int.Parse(e.CommandArgument.ToString()),
+                                                song);
+                    RefreshPlayList(sysmgr, username);
+
+                }, "Add Track to Playlist","Track has been added to the playlist");
+            }
+            
+        }
+
+    }
+}
